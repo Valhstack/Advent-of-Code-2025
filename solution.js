5083b459f6afc89335ea9c30ecb94e420e7741da
Valeriia <43146435+ValeriiaHolovatiuk@users.noreply.github.com>
2025-12-05 22:57:27 +0100

    Testing solution for part 2

--- solution.js
+++ solution.js
@@ -15,13 +15,16 @@ fetchLines().then(lines => {
 
     let ranges = splitted[0].split("\n");
     let ingredients = splitted[1].split("\n");
+    let allIds = [];
 
     let freshCount = 0;
 
-    console.log(ranges)
-    console.log(ingredients)
+    console.log(ranges);
+    console.log(ingredients);
 
-    for (let ingredient of ingredients) {
+    // Part 1 solution
+
+    /* for (let ingredient of ingredients) {
         for (let range of ranges) {
             range = range.split("-");
             console.log(range)
@@ -31,7 +34,17 @@ fetchLines().then(lines => {
                 break;
             }
         }
+    }*/
+
+    // Part 2 solution
+
+    for(let range of ranges){
+        for(let i = Number(range[0]); i <= Number(range[1]); i++){
+            allIds.push(i);
+        }
     }
 
-    console.log(freshCount);
+    let allUniqueIDs = [...new Set(allIds)];
+
+    console.log("Total count: ", allUniqueIDs.length);
 })
