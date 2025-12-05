769bb5280cf744a62c2903b7635b6393ac4a3ffa
Valeriia <43146435+ValeriiaHolovatiuk@users.noreply.github.com>
2025-12-05 22:41:39 +0100

    Testing solution for part 1 day 5

---
+++ solution.js
@@ -0,0 +1,37 @@
+async function fetchInput() {
+    return await (await fetch("input.txt")).text();
+}
+
+async function fetchLines() {
+    const response = await fetch("input.txt");
+    const lines = await response.text();
+    return lines;
+}
+
+fetchLines().then(lines => {
+    console.log(lines);
+
+    const splitted = lines.split("\n\n");
+
+    let ranges = splitted[0].split("\n");
+    let ingredients = splitted[1].split("\n");
+
+    let freshCount = 0;
+
+    console.log(ranges)
+    console.log(ingredients)
+
+    for (let ingredient of ingredients) {
+        for (let range of ranges) {
+            range = range.split("-");
+            console.log(range)
+
+            if (ingredient >= range[0] && ingredient <= range[1]) {
+                freshCount++;
+                break;
+            }
+        }
+    }
+
+    console.log(freshCount);
+})
