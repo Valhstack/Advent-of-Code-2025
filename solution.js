f06828dc92397bf9c9a40a222e37fa442b01f06c
Valeriia <43146435+ValeriiaHolovatiuk@users.noreply.github.com>
2025-12-06 21:33:07 +0100

    Testing parts of the solution for part 1 day 6

---
+++ solution.js
@@ -0,0 +1,25 @@
+async function fetchInput() {
+    return await (await fetch("input.txt")).text();
+}
+
+async function fetchLines() {
+    const response = await fetch("input.txt");
+    const text = await response.text();
+    const lines = text.split(/\r?\n/);
+
+    return lines;
+}
+
+fetchLines().then(lines => {
+    console.log(lines);
+
+    let reversedLines = [];
+
+    for(let i = 0; i < lines.length; i++){
+        for(let j = 0; j < lines[i].length; j++){
+            reversedLines.push(lines[j][i]);
+        }
+    }
+
+    console.log("Reversed: ", reversedLines)
+})
